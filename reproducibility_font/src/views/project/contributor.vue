ddc4cd5fabb9081d4659c233271061ba64f747ad
orangemimi <orangemimi@users.noreply.github.com>
2021-05-13 19:57:21 +0800

    0513

--- reproducibility_font/src/views/project/contributor.vue
+++ reproducibility_font/src/views/project/contributor.vue
@@ -2,126 +2,23 @@
 <template>
   <div class="information">
     <el-col :xs="24" :sm="24" :md="{ span: 22, offset: 1 }" :lg="{ span: 18, offset: 3 }">
-      <el-row :gutter="10">
-        <el-col :span="12">
-          <div class="info-card">
-            <div class="info-card-top">
-              <div class="info-detail">
-                <div class="content">
-                  <strong>Title:</strong>
-                  {{ projectInfo.name }}
-                </div>
-                <div class="content">
-                  <strong>Privacy:</strong>
-                  {{ projectInfo.privacy }}
-                </div>
-                <div class="content">
-                  <strong>Category:</strong>
-                  Reproduction
-                </div>
-                <div class="content">
-                  <strong style="float:left">Tag:</strong>
-                  <div v-for="(item, index) in projectInfo.tag" :key="index">
-                    <el-tag type="info" style="float:left">{{ item }}</el-tag>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <strong>Create time:</strong>
-                  {{ dateFormat(projectInfo.createTime) }}
-                </div>
-                <div class="content">
-                  <strong>Update time:</strong>
-                  {{ dateFormat(projectInfo.updateTime) }}
-                </div>
-              </div>
-
-              <div class="info-img">
-                <div style="float:right">
-                  <!-- <el-button-group> -->
-                  <el-button type="info" plain size="small" @click="editProjectInfoDialog" v-show="role == 'builder'">
-                    Edit
-                  </el-button>
-                  <!-- </el-button-group> -->
-                  <el-button-group>
-                    <el-button type="info" plain size="small">
-                      <i class="el-icon-share"></i>
-                    </el-button>
-                    <el-button type="info" plain size="small">
-                      <i class="el-icon-more"></i>
-                    </el-button>
-                  </el-button-group>
-                </div>
-                <div>
-                  <avatar username="projectInfo.name" :src="projectInfo.picture" :size="150" style="margin-top: 10px" :rounded="false" class="avatar-img"></avatar>
-                </div>
-              </div>
-            </div>
-            <div class="info-card-bottom">
-              <div class="des">
-                <strong>Description:</strong>
-                <div class="content">
-                  {{ projectInfo.description }}
-                </div>
-              </div>
-              <div class="intro">
-                <strong>Introduction:</strong>
-                <div class="content">
-                  {{ projectInfo.introduction }}
-                </div>
-              </div>
+      <div class="info-card">
+        <div class="info-card-bottom">
+          <div class="des">
+            <strong>Description:</strong>
+            <div class="content">
+              {{ projectInfo.description }}
             </div>
           </div>
-        </el-col>
-        <el-col :span="12">
-          <div class="info-card-right">
-            <div class="participants">
-              <div class="info">
-                <div class="title">Participants</div>
-                <div class="add-participant">
-                  <el-button size="small" @click="addParticipantDialogShow = true">
-                    + Add participant
-                  </el-button>
-                </div>
-              </div>
-
-              <div class="content">
-                <vue-scroll :ops="ops" class="scroll" style="height: calc(100vh - 554px);">
-                  <user-card :user="{ name: creator.name, role: 'builder' }" class=""></user-card>
-                  <div v-for="(member, index) in members" :key="index">
-                    <!-- {{ { name: member.name, role: member.role } }} -->
-                    <user-card :user="member"></user-card>
-                  </div>
-                </vue-scroll>
-              </div>
-            </div>
-            <div class="citation">
-              <div class="info">
-                <div class="title">Citation</div>
-                <div class="edit">
-                  <el-button size="small" v-show="role == 'builder'">Edit</el-button>
-                </div>
-              </div>
-
-              <!-- bind with document -->
-              <div class="content">
-                <el-input v-model="projectInfo.citation" :disabled="true"></el-input>
-              </div>
+          <div class="intro">
+            <strong>Introduction:</strong>
+            <div class="content">
+              {{ projectInfo.introduction }}
             </div>
           </div>
-        </el-col>
-      </el-row>
+        </div>
+      </div>
     </el-col>
-
-    <!-- edit the project dialog -->
-    <el-dialog title="Edit Project Info" :visible.sync="editProjectInfoDialogShow" width="40%" :close-on-click-modal="false">
-      <edit-info-form :projectInfo="projectInfo" @editProjectInfoResponse="editProjectInfoResponse"></edit-info-form>
-    </el-dialog>
-
-    <!-- share the project with email  -->
-    <el-dialog :title="'Invite a collaborator to ' + projectInfo.name" :visible.sync="addParticipantDialogShow" width="40%" :close-on-click-modal="false">
-      <share-project></share-project>
-    </el-dialog>
   </div>
 </template>
 
@@ -129,17 +26,9 @@
 import { getProjectAndUsers } from '@/api/request';
 import { dateFormat } from '@/utils/utils';
 import { mapState } from 'vuex';
-import Avatar from 'vue-avatar';
-import userCard from '_com/UserCard';
-import editInfoForm from './components/EditProjectInfo';
-import shareProject from '_com/ShareProject';
 // import reBuilderCard from '_com/UserCard/ReBuilderCard';
 export default {
   components: {
-    Avatar,
-    userCard,
-    editInfoForm,
-    shareProject
     // reBuilderCard
   },
 
@@ -220,41 +109,6 @@ export default {
     background-color: white;
     min-height: calc(100vh - 242px);
     padding: 20px 32px;
-    .info-card-top {
-      width: 100%;
-      height: 280px;
-
-      .info-detail {
-        width: 60%;
-        height: 100%;
-        float: left;
-        .content {
-          margin-bottom: 5px;
-          clear: both;
-
-          .el-tag {
-            display: inline;
-            font-size: 14px;
-            margin: 0 5px;
-          }
-        }
-      }
-
-      .info-img {
-        // width: 40%;
-        // height: 100%;
-        float: right;
-        // border: 1px solid $btnBorder;
-        .avatar-img {
-          float: right;
-          border: 1px solid $btnBorder;
-        }
-        .el-avatar {
-          float: right;
-          border: 1px solid $btnBorder;
-        }
-      }
-    }
 
     .info-card-bottom {
       width: 100%;
