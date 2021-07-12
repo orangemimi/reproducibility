b49a5ff84d6f7a4e1eb5ea8651c5d8058f962e9f
orangemimi <orangemimi@users.noreply.github.com>
2021-07-13 00:21:54 +0800

    0713

---
+++ reproducibility_font/src/config.js
@@ -0,0 +1,5 @@
+export default {
+  apiURL: '/r',
+  // containerURL: "http://127.0.0.1:8081",
+  tomcatURL: 'http://127.0.0.1:8080'
+};
---
+++ reproducibility_font/vue.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const resolve = dir => {
+  return path.join(__dirname, dir);
+};
+
+const BASE_URL = '/';
+
+module.exports = {
+  publicPath: BASE_URL,
+  css: {
+    loaderOptions: {
+      // 向 CSS 相关的 loader 传递选项
+      less: {
+        // javascriptEnabled: true
+      },
+      sass: {
+        additionalData: `@import 'src/assets/variable.scss';`
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set('@', resolve('src')).set('_com', resolve('src/components'));
+
+    config.plugin('webpack-bundle-analyzer').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
+  },
+  productionSourceMap: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: {
+      '/r': {
+        target: 'http://127.0.0.1:8081/',
+        ws: true,
+        changeOrigin: true
+        // pathRewrite: {
+        //   "^/api": ""
+        // }
+      }
+    }
+  }
+};
